53babf9b7032f50eb75fa854a9f3a8937e960bf0
malaka96 <madhubhashana655@gmail.com>
2025-08-21 17:30:46 +0530

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./assets/js/**/*.js",
+    "./assets/css/**/*.css"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
